50a6875311abe97c27521b9b3773de53187834d5
Douglas Candido <douglascandido@workmail.com>
2023-09-28 09:40:33 -0300

    Add create user dto

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  Length,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 50)
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 30)
+  @Matches(RegExp(/^[A-Za-z0-9-_]+$/))
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(8, 64)
+  @IsStrongPassword()
+  readonly password: string;
+}
