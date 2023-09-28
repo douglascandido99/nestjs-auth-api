5c2794b50566b3b37a75b2f8460a12f8b52dcd65
Douglas Candido <douglascandido@workmail.com>
2023-09-27 21:01:21 -0300

    Add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './common/prisma.service';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
