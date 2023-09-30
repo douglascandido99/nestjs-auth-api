a089e79cffb83fe3099c1f3d4d87630da9ca1efb
Douglas Candido <douglascandido@workmail.com>
2023-09-30 07:31:50 -0300

    Add complete CRUD operations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,57 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { User } from '@prisma/client';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('sign-up')
+  @Post('signup')
   async signUp(@Body() dto: CreateUserDTO) {
     return await this.userService.signUp(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.getUserById(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getAllUsers() {
+    return await this.userService.getAllUsers();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  async updateUser(@GetUser('id') id: number, @Body() dto: UpdateUserDTO) {
+    return await this.userService.updateUserInfo(id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('me')
+  async deleteUser(@GetUser('id') id: number) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,10 +2,12 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { UserRepository } from './repository/user.repository';
 import { CreateUserDTO } from './dto/create-user.dto';
 import * as argon from 'argon2';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -13,6 +15,7 @@ export class UserService {
 
   async signUp(dto: CreateUserDTO) {
     const { name, username, email, password } = dto;
+
     const [existentEmail, existentUsername] = await Promise.all([
       this.user.findUserByEmail(email),
       this.user.findUserByUsername(username),
@@ -29,11 +32,51 @@ export class UserService {
       const user = await this.user.createUser({ name, email, username, hash });
 
       const createdUser = { ...user };
+
       delete createdUser.hash;
+
       return createdUser;
     } catch (error) {
       throw new InternalServerErrorException('Something went wrong.');
     }
+  }
+
+  async getUserById(id: number) {
+    const user = await this.user.findUserById(id);
+
+    if (!user) throw new NotFoundException('User not found');
+
+    delete user.hash;
+
+    return user;
+  }
+
+  async getAllUsers() {
+    return await this.user.getAllUsers();
+  }
+
+  async updateUserInfo(id: number, dto: UpdateUserDTO) {
+    const userId = await this.user.findUserById(id);
+
+    if (!userId) throw new NotFoundException('User not found.');
+
+    const user = await this.user.updateUser(id, dto);
+
+    const updatedUser = { ...user };
+
+    delete updatedUser.hash;
+
+    return updatedUser;
+  }
+
+  async deleteUser(id: number) {
+    const userId = await this.user.findUserById(id);
+
+    if (!userId) throw new NotFoundException('User not found');
+
+    await this.user.deleteUser(id);
+
+    return { msg: 'User deleted.' };
   }
 
   private async hashPasword(password: string) {
