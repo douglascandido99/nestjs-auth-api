52e41a36a9e5c304acd8a4357a6903210ff1954e
Douglas Candido <douglascandido@workmail.com>
2023-09-30 07:32:00 -0300

    Add update user dto

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDTO } from './create-user.dto';
+
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {
+  readonly name?: string;
+  readonly email?: string;
+  readonly username?: string;
+}
