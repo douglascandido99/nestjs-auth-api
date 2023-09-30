0c00ac8be983c300f3f0fb9b1620909743d6cd7a
Douglas Candido <douglascandido@workmail.com>
2023-09-30 14:01:27 -0300

    Update modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
-import { AuthRepository } from './repository/auth.repository';
 import { UserModule } from 'src/user/user.module';
 import { UserRepository } from 'src/user/repository/user.repository';
 import { PrismaService } from 'src/common/prisma.service';
 import { PassportModule } from '@nestjs/passport';
+import { JwtRepository } from './repository/jwt.repository';
+import { AuthRepository } from './repository/auth.repository';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PassportModule } from '@nestjs/passport';
   controllers: [AuthController],
   providers: [
     AuthService,
+    JwtRepository,
     AuthRepository,
     UserRepository,
     PrismaService,
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './repository/user.repository';
 import { PrismaService } from 'src/common/prisma.service';
+import { AuthRepository } from 'src/auth/repository/auth.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserRepository, PrismaService],
+  providers: [UserService, UserRepository, PrismaService, AuthRepository],
   exports: [UserService, UserRepository],
 })
 export class UserModule {}
