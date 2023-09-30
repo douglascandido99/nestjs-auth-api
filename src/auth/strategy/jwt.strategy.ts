0d9c20baa4f20db0721a7b8eabe6b07adb214171
Douglas Candido <douglascandido@workmail.com>
2023-09-30 07:31:14 -0300

    Fix jwt strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from 'src/user/repository/user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly user: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     });
   }
 
-  async validate(token: any) {
-    return { id: token.id, u: token.u, e: token.e };
+  async validate(payload: { id: number; email: string; username: string }) {
+    try {
+      const user = await this.user.findUserById(payload.id);
+      if (user) {
+        delete user.hash;
+        return user;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'An error occurred while validating the JWT token.',
+      );
+    }
   }
 }
